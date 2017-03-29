04f4525684a66ecec0b9e4511c14df17d5bec10c
Muhammad Tayyab <tayyabmuhammad152@gmail.com>
2017-03-29 12:57:11 +0500

    hello
    chk the file

---
+++ app/src/main/java/ict_trainings/ictapp/Main2Activity.java
@@ -0,0 +1,16 @@
+package ict_trainings.ictapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ict_trainigs.ictapp.R;
+
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
