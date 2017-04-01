e7c3bc77336b2690b2cafb6b91c1d3ff405713b7
faizanalibutt <mfaizanalibutt@gmail.com>
2017-04-01 17:09:52 +0500

    New Fragment Added
    Fragment of Course

--- app/src/main/java/ict_trainings/ictapp/ICThome.java
+++ app/src/main/java/ict_trainings/ictapp/ICThome.java
@@ -2,6 +2,7 @@ package ict_trainings.ictapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -73,7 +74,7 @@ public class ICThome extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
