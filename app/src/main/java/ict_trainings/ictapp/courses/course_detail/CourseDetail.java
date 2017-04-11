e908fab71d99c5befcf3fee16ea0f0765af7e0c0
Muhammad Tayyab <tayyabmuhammad152@gmail.com>
2017-04-11 12:45:38 +0500

    expendable list view
    expendable list view in course details has been integrated...

--- app/src/main/java/ict_trainings/ictapp/courses/course_detail/CourseDetail.java
+++ app/src/main/java/ict_trainings/ictapp/courses/course_detail/CourseDetail.java
@@ -14,6 +14,14 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import com.squareup.okhttp.OkHttpClient;
 
@@ -30,6 +38,10 @@ import static ict_trainings.ictapp.courses.helper.fragment.Course.PHOTOS_BASE_UR
 
 
 public class CourseDetail extends AppCompatActivity {
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     private OkHttpClient okHttpClient = new OkHttpClient();
     Course course;
@@ -41,8 +53,9 @@ public class CourseDetail extends AppCompatActivity {
         setContentView(R.layout.activity_course_detail);
 
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-        if(actionBar != null){
-            actionBar.setDisplayHomeAsUpEnabled(true);
+        if (actionBar != null) {
+            actionBar.hide();
+//            actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
         Bundle bundle = getIntent().getBundleExtra
@@ -60,7 +73,7 @@ public class CourseDetail extends AppCompatActivity {
 //                textTitle.setText(course.getCourseTitle());
 //                TextView textDesc = (TextView) findViewById(R.id.desc);
 //                textDesc.setText(course.getCourseDes());
-                Toast.makeText(this,"loading....", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "loading....", Toast.LENGTH_SHORT).show();
             } else {
 //                TextView textTitle = (TextView) findViewById(R.id.title);
 //                textTitle.setText(course.getCourseTitle());
@@ -72,34 +85,213 @@ public class CourseDetail extends AppCompatActivity {
                 loader.execute(container);
             }
         }
+
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+        // Listview Group click listener
+        expListView.setOnGroupClickListener(new OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                // Toast.makeText(getApplicationContext(),
+                // "Group Clicked " + listDataHeader.get(groupPosition),
+                // Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+
+            }
+        });
+
+        // Listview Group collasped listener
+        expListView.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+
+
+            }
+        });
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+
+                return false;
+            }
+        });
+    }
+
+    /*
+         * Preparing the list data
+         */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("The Basics");
+        listDataHeader.add("Fundamentals of Java");
+        listDataHeader.add("Unit Testing and Debugging");
+        listDataHeader.add("Android User Interface");
+        listDataHeader.add("Styling Widgets");
+        listDataHeader.add("Layout");
+        listDataHeader.add("Advanced Widgets");
+        listDataHeader.add("Storing and Retrieving Data");
+        listDataHeader.add("Content Providers");
+        listDataHeader.add("Asynchronous Tasks");
+        listDataHeader.add("Location Services and Maps");
+        listDataHeader.add("WebView");
+        listDataHeader.add("Best Practices");
+        listDataHeader.add("Final Project");
+
+        // Adding child data
+        List<String> a = new ArrayList<String>();
+        a.add("What is Android?");
+        a.add("Architecture Explained");
+        a.add("Role of Java");
+
+
+        List<String> b = new ArrayList<String>();
+        b.add("What is Java?");
+        b.add("Fundamentals of Java");
+        b.add("Android SDK");
+        b.add("Eclipse IDE Plugin");
+        b.add("Device Emulator");
+        b.add("Profiling Tools");
+        b.add("Hello World Application");
+
+        List<String> c = new ArrayList<String>();
+        c.add("Creating Unit Tests");
+        c.add("Android Development Tools (ADT)");
+        c.add("Using the Emulator");
+        c.add("Analyzing the Heap");
+
+        List<String> d = new ArrayList<String>();
+        d.add("View Hierarchy");
+        d.add("Menus");
+        d.add("Fragments");
+
+        List<String> e = new ArrayList<String>();
+        e.add("Defining Styles");
+        e.add("Applying Styles to the UI");
+        e.add("Platform Styles and Themes");
+
+        List<String> f = new ArrayList<String>();
+        f.add("Layout Containers");
+        f.add("Weight and Gravity");
+        f.add("Layout TechniquesHandling Events");
+
+        List<String> g = new ArrayList<String>();
+        g.add("Scroll View");
+        g.add("ViewPager");
+        g.add("TabView");
+        g.add("Custom Views");
+
+        List<String> h = new ArrayList<String>();
+        h.add("Internal and External Storage ");
+        h.add("Preferences");
+
+        List<String> i = new ArrayList<String>();
+        i.add("Querying Content Providers  ");
+        i.add("Modifying Data ");
+        i.add("Creating a Content Provider ");
+
+        List<String> j = new ArrayList<String>();
+        j.add("Main UI Thread  ");
+        j.add("Using AsyncTask  ");
+
+        List<String> k = new ArrayList<String>();
+        k.add("Location Services  ");
+        k.add("Mock Location Data  ");
+        k.add("Google Map Libraries  ");
+
+        List<String> l = new ArrayList<String>();
+        l.add("Web Apps Overview ");
+        l.add("Target Screens from Web Apps ");
+        l.add("WebView  ");
+        l.add("Debugging Web Apps  ");
+        l.add("Best Practices for web apps  ");
+
+        List<String> m = new ArrayList<String>();
+        m.add("Compatibility ");
+        m.add("Supporting multiple screens ");
+        m.add("Optimizing for Other Android Versions ");
+
+        List<String> n = new ArrayList<String>();
+        n.add("Final Project ");
+
+        listDataChild.put(listDataHeader.get(0), a);
+        listDataChild.put(listDataHeader.get(1), b);
+        listDataChild.put(listDataHeader.get(2), c);
+        listDataChild.put(listDataHeader.get(3), d);
+        listDataChild.put(listDataHeader.get(4), e);
+        listDataChild.put(listDataHeader.get(5), f);
+        listDataChild.put(listDataHeader.get(6), g);
+        listDataChild.put(listDataHeader.get(7), h);
+        listDataChild.put(listDataHeader.get(8), i);
+        listDataChild.put(listDataHeader.get(9), j);
+        listDataChild.put(listDataHeader.get(10),k);
+        listDataChild.put(listDataHeader.get(11),l);
+        listDataChild.put(listDataHeader.get(12),m);
+        listDataChild.put(listDataHeader.get(13),n);
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_network, menu);
-        return super.onCreateOptionsMenu(menu);
+    public void onBackPressed() {
+        finish();
+        Intent intent = new Intent(CourseDetail.this, ICThome.class);
+        startActivity(intent);
+        super.onBackPressed();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int i = item.getItemId();
-        switch(i){
-            case android.R.id.home:
-                finish();
-                Intent home = new Intent(CourseDetail.this, ICThome.class);
-                startActivity(home);
-                return super.onOptionsItemSelected(item);
-        }
-        return false;
-    }
+    //    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.menu_network, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int i = item.getItemId();
+//        switch (i) {
+//            case android.R.id.home:
+//                finish();
+//                Intent home = new Intent(CourseDetail.this, ICThome.class);
+//                startActivity(home);
+//                return super.onOptionsItemSelected(item);
+//        }
+//        return false;
+//    }
 
     class CourseView {
         Bitmap bitmap;
         Course course;
     }
 
-    private class CourseImage extends AsyncTask<CourseView, Void, CourseView> {
+    public class CourseImage extends AsyncTask<CourseView, Void, CourseView> {
         @Override
         protected CourseView doInBackground(CourseView... params) {
             CourseView courseVIew = params[0];
@@ -133,3 +325,6 @@ public class CourseDetail extends AppCompatActivity {
         }
     }
 }
+
+
+
