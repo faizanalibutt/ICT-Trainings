688000b4f6a47462b90567aba792d0574dc31558
faizanalibutt <mfaizanalibutt@gmail.com>
2017-05-25 15:14:52 +0500

    courses

--- app/src/main/java/ict_trainings/ictapp/courses/latest_courses/latest_courses.java
+++ app/src/main/java/ict_trainings/ictapp/courses/latest_courses/latest_courses.java
@@ -3,16 +3,13 @@ package ict_trainings.ictapp.courses.latest_courses;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import ict_trainigs.ictapp.R;
 import ict_trainings.ictapp.ICThome;
 
-public class latest_courses extends AppCompatActivity {
+public class Latest_Courses extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +19,13 @@ public class latest_courses extends AppCompatActivity {
         if(actionBar != null){
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-
-
-
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
         TextView eveTxt = (TextView) findViewById(R.id.eveTxt);
         if(extras != null) {
-            String notify = extras.getString(latest_course_Service.TAG);
-            Log.d(latest_course_Service.TAG, notify + " coming notify");
-            eveTxt.setText(notify);
+//            String notify = extras.getString(latest_course_Service.TAG);
+//            Log.d(latest_course_Service.TAG, notify + " coming notify");
+//            eveTxt.setText(notify);
         }
 
     }
@@ -41,7 +35,7 @@ public class latest_courses extends AppCompatActivity {
         switch(i){
             case android.R.id.home:
                 finish();
-                Intent home = new Intent(latest_courses.this, ICThome.class);
+                Intent home = new Intent(Latest_Courses.this, ICThome.class);
                 startActivity(home);
                 return super.onOptionsItemSelected(item);
         }
